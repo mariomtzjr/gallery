5407e076d214ed02ae1ff6ab10337528d7528403
mariomtzjr <francisco.smtz@hotmail.com>
2020-03-15 14:13:51 -0600

    Fixed circular dependency on album model

--- album/models.py
+++ album/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from photos.models import Photo
+from photo.models import Photo
 
 
 # Create your models here.
 class Album(models.Model):
     title = models.CharField(max_length=100)
-    photos = models.ManyToManyField(Photo)
+    photos = models.ManyToManyField(Photo, related_name='+')
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
