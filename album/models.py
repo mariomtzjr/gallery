e21ed012a286adc3fd9dcf57fdb4621d0ccf27e3
mariomtzjr <francisco.smtz@hotmail.com>
2020-03-15 16:30:40 -0600

    Added type of cover for album

--- album/models.py
+++ album/models.py
@@ -4,7 +4,16 @@ from photo.models import Photo
 
 # Create your models here.
 class Album(models.Model):
+    COVER_CHOICES = (
+            ('SC', 'SOFT COVER'),
+            ('HC', 'HARD COVER'),
+    )
     title = models.CharField(max_length=100)
+    cover = models.CharField(
+        max_length=20,
+        choices=COVER_CHOICES,
+        default='SOFT COVER',
+    )
     photos = models.ManyToManyField(Photo, related_name='+')
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
