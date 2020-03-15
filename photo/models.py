614d20797f9cd8a5276e4623b92cac8966351e87
mariomtzjr <francisco.smtz@hotmail.com>
2020-03-15 15:17:08 -0600

    Modified image field of Photo model

--- photo/models.py
+++ photo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Photo(models.Model):
-    url = ""
+    image = models.ImageField(upload_to='gallery')
     caption = models.CharField(max_length=100)
     album = models.ForeignKey('album.Album', on_delete=models.CASCADE)
     created_at = models.DateField(auto_now_add=True)
