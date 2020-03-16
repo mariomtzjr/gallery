f2ed5c4408515d1767ec69c50bbd731edc521c7c
mariomtzjr <francisco.smtz@hotmail.com>
2020-03-15 18:50:46 -0600

    Fixed action to delete an album

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^album/listar/$', views.AlbumList.as_view(), name='album_list'),
     url(r'^album/crear/$', views.AlbumCreate.as_view(), name='album_create'),
     url(r'^album/editar/$', views.AlbumUpdate.as_view(), name='album_update'),
-    url(r'^album/eliminar/$', views.AlbumDelete.as_view(), name='album_delete')
+    url(r'^album/eliminar/(?P<pk>[0-9]+)/$', views.AlbumDelete.as_view(), name='album_delete')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
