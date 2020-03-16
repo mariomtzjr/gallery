c0044cc945edc4ba9dc865eaed11f00bb1e5c29f
mariomtzjr <francisco.smtz@hotmail.com>
2020-03-15 22:44:05 -0600

    Added logic for upload photos from album in admin site

--- album/admin.py
+++ album/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from album.models import Album
+from photo.models import Photo
 
 
 # Register your models here.
+class PhotoInline(admin.TabularInline):
+    model = Photo
+    extra = 0
+
+
 @admin.register(Album)
 class AlbumAdmin(admin.ModelAdmin):
+    inlines = [
+        PhotoInline,
+    ]
     list_display = ('title', 'created_at',)
